2fbcfb0831122f91df2073231746fd29ed439e3b
Cristian Araujo <araujocristian@live.com>
2022-02-07 22:17:10 -0300

    feat: Fazendo fetch nos dados da API da GoG

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -6,8 +6,11 @@
  */
 
 module.exports = {
-  populate: (ctx) => {
-    console.log("Initializing");
-    ctx.send({ ok: true });
+  populate: async (ctx) => {
+    console.log("Starting to populate...");
+
+    await strapi.services.game.populate();
+
+    ctx.send("Finished populating!");
   },
 };
